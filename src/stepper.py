7f66631421f28249bc0a28b56da9d22b0fab2fa5
annsudhart <sudharta.andrea@gmail.com>
2018-11-18 16:42:04 -0800

    Started on stepper motor code

---
+++ src/stepper.py
@@ -0,0 +1,50 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+enable_pin = 18
+coil_A_1_pin = 4
+coil_A_2_pin = 17
+coil_B_1_pin = 23
+coil_B2_pin = 24
+
+GPIO.setup(enable_pin, GPIO.OUT)
+GPIO.setup(coil_A_1_pin, GPIO.OUT);
+GPIO.setup(coil_A_2_pin, GPIO.OUT);
+GPIO.setup(coil_B_1_pin, GPIO.OUT);
+GPIO.setup(coil_B_2_pin, GPIO.OUT);
+
+GPIO.output(enable_pin, 1)
+
+def forward(delay, steps):
+    for i in range(0, steps):
+        setStep(1, 0, 1, 0)
+        time.sleep(delay)
+        setStep(0, 1, 1, 0)
+        time.sleep(delay)
+        setStep(0, 1, 0, 1)
+        time.sleep(delay)
+        setStep(1, 0, 0, 1)
+
+
+def backwards(delay, steps):
+    for i in range(0, steps):
+        setStep(1, 0, 0, 1)
+        time.sleep(delay)
+        setStep(0, 1, 0, 1)
+        time.sleep(delay)
+        setStep(0, 1, 1, 0)
+        time.sleep(delay)
+        setStep(1, 0, 1, 0)
+
+def setStep(w1, w2, w3, w4):
+    GPIO.output(coil_A_1_pin, w1)
+    GPIO.output(coil_A_2_pin, w2)
+    GPIO.output(coil_B_1_pin, w3)
+    GPIO.output(coil_B_2_pin, w4)
+
+while True:
+    delay = 1
+    steps = 128 #128 = 90 degree rotation
+    
