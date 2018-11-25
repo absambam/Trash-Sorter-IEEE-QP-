eb9848d3ea9f9b0c45c9237cb901b75b4dd53014
Abirami Sambamoorthy <36246781+absambam@users.noreply.github.com>
2018-11-24 22:21:46 -0800

    fixed path to save image to

--- src/camera.py
+++ src/camera.py
@@ -4,7 +4,7 @@ from time import sleep, gmtime, strftime
 camera = PiCamera()
 
 def take_picture():
-    output = strftime("/home/pi/ts36/images/image-%d-%m_%H:%M.jpg", gmtime())
+    output = strftime("/home/pi/ts36/tf_files/images/image-%d-%m_%H:%M.jpg", gmtime())
     
     camera.start_preview()
     sleep(3)
