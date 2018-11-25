965f75aa506046f0c00100a3303e3c95cd67075e
Abirami Sambamoorthy <36246781+absambam@users.noreply.github.com>
2018-11-24 22:06:48 -0800

    fixed camera.py
    previous version wasn't working because some lines were outside of the function, so changed it to add more lines into the function so that it would work

--- src/camera.py
+++ src/camera.py
@@ -3,12 +3,12 @@ from time import sleep, gmtime, strftime
 
 camera = PiCamera()
 
-camera.resolution = (800, 480)
-camera.hflip = True
-camera.start_preview(alpha=128)
-
-output = strftime("/home/pi/ts-images/image-%d-%m %H:%M.jpg", gmtime())
-
 def take_picture():
+    output = strftime("/home/pi/ts36/images/image-%d-%m_%H:%M.jpg", gmtime())
+    
+    camera.start_preview()
+    sleep(3)
     camera.capture(output)
     camera.stop_preview()
+
+take_picture()
