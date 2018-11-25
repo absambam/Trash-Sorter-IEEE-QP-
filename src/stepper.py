9bcc42ec58f3b4707b213ca7b483097d0b270bef
annsudhart <sudharta.andrea@gmail.com>
2018-11-25 15:22:06 -0800

    rotate function now takes in name of bin to rotate to

--- src/stepper.py
+++ src/stepper.py
@@ -9,6 +9,17 @@ coil_A_2_pin = 17
 coil_B_1_pin = 23
 coil_B2_pin = 24
 
+# constants
+num_bins = 3
+delay = 0.25
+steps = 171 #170.666666
+total_steps = 512
+
+# global vars
+current_bin = 0 #trash = 0, recycling = 1, compost = 2
+position = 0
+
+
 GPIO.setup(enable_pin, GPIO.OUT)
 GPIO.setup(coil_A_1_pin, GPIO.OUT);
 GPIO.setup(coil_A_2_pin, GPIO.OUT);
@@ -44,12 +55,23 @@ def setStep(w1, w2, w3, w4):
     GPIO.output(coil_B_1_pin, w3)
     GPIO.output(coil_B_2_pin, w4)
 
-# rotate 1, 2, 3, or 4 * 90 degrees
-# rotations is a value between 1 to 4 corresponding to the number of quarters
-# to rotate.
-def rotate( int rotations )
-    delay = .25
-    steps = 128 #128 = 90 degree rotation
+# rotate 1, 2, or 3 * 120 degrees
+# bin_name is a string with any of the three values trash, recycling, or compost
+
+def rotate( bin_name ):
+    destination = 0
+    if bin_name == "recycling":
+        destination = 1
+    elif bin_name == "compost":
+        destination = 2
+    rotations = ((360 + (destination - current_bin)*120) % 360) / 120
+    current_bin = rotations + current_bin
     print("Preparing to move ", rotations*steps, " steps...")
     forward(delay, steps*rotations)
-    print("Moved " rotations*steps, " steps")
+    if current_bin >= num_bins:
+        backwards(delay, 1)
+        current_bin -= num_bins
+    position += rotations*steps
+    if position > total_steps: #prevent going over 512 degrees
+        position -= total_steps
+    print("Moved " rotations*steps, " steps. Now at position ", position, "steps and at bin ", current_bin)
